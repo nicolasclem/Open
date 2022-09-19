80c0e5379a46a505d719875f0a87038d5af293ad
Nicolas Clementi <clementinicolas@gmail.com>
2022-09-19 15:32:20 -0300

    entrega 1 y 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+// ej N° 1
+
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hello, " + nombre);
+
+
+// ej N°2
+
+var Hora = DateTime.Now;
+
+Console.WriteLine("Fecha y hora actual :" + Hora.ToString("F"));
+
