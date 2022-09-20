479ec3c8d8d28bdbd8b7a4891754ff84353eb288
Nicolas Clementi <clementinicolas@gmail.com>
2022-09-20 04:03:56 -0300

    Ejercicios 1 , 2 y 3

--- Program.cs
+++ Program.cs
@@ -1,16 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+//Ej N°1
+Console.WriteLine("=========*****EJ N°1*****=======");
+
+Console.WriteLine("Dime tu nombre: ");
+string name = Console.ReadLine();
+Console.WriteLine("Dime tu edad:");
+int age =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Cual es tu peso actual :  (kg, grs)");
+float pesoActual = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Tu nombre es " + name +" tienes " +age+ " años de edad"+ " y tu peso actual es de " + pesoActual +" kg");
+
+//Ej N°2
+Console.WriteLine("=========*****EJ N°2*****=======");
+
+//coche
+
+int puertas = 2;
+    int ruedas = 4;
+    string marca = "Toyota";
+    bool itv = false;
+
+// mesa
+
+    float peso = 12.850f;
+    int largo = 235;//en centimetros;
+    string material = "madera";
+    string color = "Marron";
 
 
-// ej N° 1
+Console.WriteLine("Nuestro coche tiene: " + puertas + " puertas "+ ruedas  + " ruedas "+ " es marca " + marca + " y el estadod e ITV es : " + itv);
 
-Console.WriteLine("Introduce tu nombre: ");
-string nombre = Console.ReadLine();
-Console.WriteLine("Hello, " + nombre);
+Console.WriteLine("Descripcion de mi mesa : " + peso + "kg " + largo + "cm " + " es de :  " + material + " color: " + color);
 
 
-// ej N°2
+//Ej N°3
 
-var Hora = DateTime.Now;
+int num = 15;
+char a = 'b';
 
-Console.WriteLine("Fecha y hora actual :" + Hora.ToString("F"));
 
+int num1 = 20;
+char b = 'a';
+
+Console.WriteLine("=========*****EJ N°3*****=======");
+
+Console.WriteLine(num >= 18);
+Console.WriteLine(num1 >= 18);
+Console.WriteLine(a == 'a');
+Console.WriteLine(b == 'a');
+Console.WriteLine((num1 >=18)&&(b=='a'));
+Console.WriteLine((num1 >= 18) || (a == 'a'));
