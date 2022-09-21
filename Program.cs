8e52b0379baf07daf30df2c0043b50df893323b8
Nicolas Clementi <clementinicolas@gmail.com>
2022-09-21 11:00:11 -0300

    ejercicios lecc 4

--- Program.cs
+++ Program.cs
@@ -1,16 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//while
+Console.WriteLine("Indica un numero ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+Console.WriteLine("==============Inicio de tablas=========");
+while (i <= 10)
+{
+    Console.WriteLine(i * num);
+    i++;
+
+}
 
 
-// ej N° 1
+//do while
 
-Console.WriteLine("Introduce tu nombre: ");
-string nombre = Console.ReadLine();
-Console.WriteLine("Hello, " + nombre);
+Console.WriteLine("\n==============Ej 2=========");
+int p = 0;
+int Result = 0;
+Console.WriteLine("Dime un numero :");
+int numDW = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("=================== Contadores!!!=====================" +
+    "");
+if (numDW > 0)
+{
+    do
+    {
+        Result += p;
+        p++;
+    } while (p <= numDW);
+}
+else
+{
+    do
+    {
+        Result += p;
+        p--;
+
+    } while (p >= numDW);
+}
+Console.WriteLine(Result);
 
 
-// ej N°2
+//for
 
-var Hora = DateTime.Now;
+Console.WriteLine("Alto: ");
+int altura = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ancho: ");
+int anchura = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Relleno:  (True o False)");
+bool relleno = Convert.ToBoolean(Console.ReadLine());
 
-Console.WriteLine("Fecha y hora actual :" + Hora.ToString("F"));
+
+if (relleno == false)
+{
+    for (int x = 1; x <= altura; x++)
+    {
+        for (int j = 1; j <= anchura; j++)
+        {
+            if ((x == 1 || x == altura) || (j == 1 || j == anchura))
+            {
+                Console.Write(" * ");
+            }
+            else
+            {
+                Console.Write("   ");
+            }
+        }
+        Console.WriteLine(" ");
+    }
+}
+else
+{
+    for (int x = 1; x <= altura; x++)
+    {
+        for (int j = 1; j <= anchura; j++)
+        {
+            Console.Write(" * ");
+        }
+        Console.WriteLine(" ");
+    }
+}
 
